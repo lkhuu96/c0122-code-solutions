a4c0dc3c1fa282104e574e0c27b6ebf255bcd79e
Luc-Ky Khuu <khuu96@gmail.com>
2022-01-31 11:43:00 -0800

    completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 5;
+var height = 8;
+var area = width * height;
+console.log('value of area:', area);
+console.log('type of area:', typeof area);
+
+var bill = 10;
+var payment = 14;
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('type of change:', typeof change);
+
+var quizzes = 5;
+var midterm = 18;
+var final = 20;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('type of grade:', typeof grade);
+
+var firstName = 'Luc-Ky';
+var lastName = 'Khuu';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('type of fullName:', typeof fullName);
+
+var pH = 10;
+var isAcidic = (pH < 7);
+console.log('value of isAcidic:', isAcidic);
+console.log('type of isAcidic:', typeof isAcidic);
+
+var headCount = 18;
+var isSparta = (headCount === 300);
+console.log('value of isSpart:', isSparta);
+console.log('type of isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('type of motto:', typeof motto);
