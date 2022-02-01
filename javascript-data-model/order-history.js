71088cd5090bef088f010f0474177812a39f4c23
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-01 11:06:45 -0800

    completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,90 @@
+var orderHistory = [
+  {
+    order: '113-2883177-2648248',
+    orderDate: 'July 3, 2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    item: [
+      {
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        authorLast: null,
+        authorFirst: null,
+        delivered: 'Jul 5, 2020',
+        droppedWhere: null,
+        returnWindow: 'Aug 4, 2020',
+        price: '$94.95',
+        support: true,
+        review: true
+      },
+      {
+        name: 'The Art of SQL',
+        authorLast: 'Faroult',
+        authorFirst: 'Stephanie',
+        delivered: 'Jul 5, 2020',
+        droppedWhere: null,
+        returnWindow: 'Aug 4, 2020',
+        price: '$33.99',
+        support: false,
+        review: false
+      }
+    ]
+  },
+  {
+    order: '114-2875557-9059409',
+    orderDate: 'July 4, 2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    item: [
+      {
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        authorLast: null,
+        authorFirst: null,
+        delivered: 'Jul 7, 2020',
+        droppedWhere: 'Your package was delivered. It was handed directly to a resident',
+        returnWindow: 'Aug 5, 2020',
+        price: '$15.98',
+        support: false,
+        review: true
+      }
+    ]
+  },
+  {
+    order: '113-9984268-1280257',
+    orderDate: 'July 19, 2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    item: [
+      {
+        name: 'The Timeless Way of Building',
+        authorLast: 'Alexander',
+        authorFirst: 'Christopher',
+        delivered: 'Jul 20, 2020',
+        droppedWhere: 'Your package was delivered. It was handed directly to a resident',
+        returnWindow: 'Aug 19, 2020',
+        price: '$41.33',
+        support: false,
+        review: true
+      }
+    ]
+  },
+  {
+
+    order: '114-3941689-8772232',
+    orderDate: 'August 4, 2020',
+    total: '$34.00',
+    shipTo: 'JS Masher',
+    item: [
+      {
+        name: 'JavaScript for impatient programmers',
+        authorLast: 'Rauschmayer',
+        authorFirst: 'Dr. Axel',
+        delivered: 'Jul 20, 2020',
+        deliveredWhere: null,
+        returnWindow: 'Sep 7, 2020',
+        price: '$31.55',
+        support: false,
+        review: true
+      }
+    ]
+  }
+];
