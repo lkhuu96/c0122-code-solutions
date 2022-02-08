0addd569bb142ac3cb3294c1ba237a3b7b1835d2
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-07 17:29:48 -0800

    completed object-manipulation-2 exercise

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,33 @@
 /* exported defaults */
+/*
+function takes two objects and adds any key-value pair from the second object to the first
+  object if it is not there
+create new object and set equal to target
+if key is in source, but not in target, add key value to new object
+create counter to keep track of if source key is already in target
+need to loop source keys through target object and see if keys match
+first loop to loop through source keys
+second loop to through the object keys and match the source keys
+if keys match, add to counter
+if counter is greater than 0 (meaning key match was found), reset counter and
+  continue to next iteration
+if counter is 0 (meaning no match), add source key-value pair to new object holding
+  target object.
+*/
+
+function defaults(target, source) {
+  var newObj = target;
+  var count = 0;
+  for (var x in source) {
+    for (var y in target) {
+      if (x === y) {
+        count++;
+      }
+    }
+    if (count > 0) {
+      count = 0;
+    } else {
+      newObj[x] = source[x];
+    }
+  }
+}
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -2,19 +2,29 @@
 /*
 return an object with key value pair(s) not in a given array of keys
 create empty object to hold new key value pair(s)
-loop array of keys through object keys
-if object key matches array element, skip
-if array element is not in object key, add to object
+create counter to keep track of if key is in array
+loop properties of the object through the given keys array
+use for in loop to loop through the keys in object
+use for loop to loop object keys through the keys array
+if object key matches element in keys array, add to counter
+if counter is greater than 0, reset counter and loop next object key
+if counter is 0, add key-value pair to empty object
 return object
-
 */
 
 function omit(source, keys) {
   var newObj = {};
-  // var isInArray = false;
-  for (var y = 0; y < keys.length; y++) {
-    if (!source[keys[y]]) {
-      newObj[keys[y]] = source[keys[y]];
+  var count = 0;
+  for (var key in source) {
+    for (var x = 0; x < keys.length; x++) {
+      if (keys[x] === key) {
+        count++;
+      }
+    }
+    if (count > 0) {
+      count = 0;
+    } else {
+      newObj[key] = source[key];
     }
   }
   return newObj;
