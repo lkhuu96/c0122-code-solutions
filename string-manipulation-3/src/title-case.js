966930ecc584a76fd67dc19f09baf396b5c22e22
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-09 10:40:11 -0800

    string-manipulation-3 title-case.js in progress

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -2,25 +2,28 @@
 /*
 return string with every word capitalized, excluding minor words, JavaScript, and API
 make entire string lower case
-create empty string variable to hold words (temporarily)
-loop through string, adding each char to a temporary word container
-if there is a space, check if word is preceded by a colon
-if word has colon two indexes prior, capitalize and add to new string variable
-if word does not have a colon two indexes prior, check if word is in minor array
-if word is in minor array, add to string variable as lowercase
-coninue loop and repeat until end of string has been reached.
-return the new string with title case
+create array with 'minor' words
+loop through string chars and capitalize the first letter and every other letter
+  after a space or a '-'.
+split string into array using .split(' ')
+loop through each item in the array.
+check if array element matches minor word
+if array element is minor word, make lower case unless word before it ends with colon
+check if array element is javascript or api, if true then convert to JavaScript or API
+concat array elements
 */
 
 // function titleCase(title) {
-//   var titleCase = '';
+//   var titleCase = title[0].toUpperCase();
 //   var lowerTitle = title.toLowerCase();
-//   var tempWord = '';
-//   for (var x = 0; x < title.length; x++) {
-//     if (title[x - 1] === ' ') {
-//       if (title[x - 2] === ':') {
-//         tempWord += title[x].toUpperCase();
-//       }
+//   var titleCaseArray = [];
+//   for (var x = 1; x < lowerTitle.length; x++) {
+//     if (lowerTitle[x - 1] === ' ' || lowerTitle[x - 1] === '-') {
+//       titleCase += lowerTitle[x].toUpperCase();
+//     } else {
+//       titleCase += lowerTitle[x];
 //     }
 //   }
+//   console.log(titleCase);
+
 // }
