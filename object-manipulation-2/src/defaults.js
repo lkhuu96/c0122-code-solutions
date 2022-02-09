26b9cf2bfcc9ad7b1314acbe9e26edf4a7563dcc
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-09 10:22:27 -0800

    removed if-statement from i nvert.js, removed counter and reduced code to one for-loop in default.js

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -3,31 +3,19 @@
 function takes two objects and adds any key-value pair from the second object to the first
   object if it is not there
 create new object and set equal to target
-if key is in source, but not in target, add key value to new object
-create counter to keep track of if source key is already in target
-need to loop source keys through target object and see if keys match
-first loop to loop through source keys
-second loop to through the object keys and match the source keys
-if keys match, add to counter
-if counter is greater than 0 (meaning key match was found), reset counter and
-  continue to next iteration
-if counter is 0 (meaning no match), add source key-value pair to new object holding
-  target object.
+loop through keys in source
+if target object has matching key, skip
+if target does not have a matching key, or matching key is undefined, add source key-value
+  pair to newObj.
+  return newObj
 */
 
 function defaults(target, source) {
   var newObj = target;
-  var count = 0;
   for (var x in source) {
-    for (var y in target) {
-      if (x === y) {
-        count++;
-      }
-    }
-    if (count > 0) {
-      count = 0;
-    } else {
+    if (target[x] === undefined) {
       newObj[x] = source[x];
     }
   }
+  return newObj;
 }
--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -3,7 +3,6 @@
 return an object that swaps the keys and values (original keys become values and
   original values become keys)
 create an empty object to store the new key value pairs (newObj)
-need to check if any data is in the given object
 loop through given object
 add key value pairs to new object
 return the new object
@@ -11,10 +10,8 @@ return the new object
 
 function invert(source) {
   var newObj = {};
-  if (source) {
-    for (var keys in source) {
-      newObj[source[keys]] = keys;
-    }
+  for (var keys in source) {
+    newObj[source[keys]] = keys;
   }
   return newObj;
 }
