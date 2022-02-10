e5e4cd80ec0a6e7af326c8d1e2679b77d8744795
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-10 11:53:58 -0800

    changed lightSwitch conditional to a boolean

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -2,13 +2,16 @@
 var $lightBulb = document.querySelector('.light-bulb-on');
 var $background = document.querySelector('.background-on');
 $lightBulb.addEventListener('click', lightSwitch);
+var isLightOn = true;
 
 function lightSwitch(event) {
-  if (event.target.className === 'light-bulb-shape light-bulb-on') {
+  if (isLightOn === true) {
     $lightBulb.className = 'light-bulb-shape light-bulb-off';
     $background.className = 'background-off';
+    isLightOn = false;
   } else {
     $lightBulb.className = 'light-bulb-shape light-bulb-on';
     $background.className = 'background-on';
+    isLightOn = true;
   }
 }
