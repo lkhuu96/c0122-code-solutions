090a55b6ab78b82849324e44e3f8313fbc16da1f
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-10 09:58:49 -0800

    flatten.js pseudo code in progress

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,11 @@
 /* exported flatten */
+/*
+unpack any passed arrays by 1 level
+loop through original array
+if any index value is an array (toFlatten)
+splice original array at toFlatten index, delete its original position
+add the all toFlatten values
+the number to be inserted should be toFlatten.length
+the values to be inserted should be in a for loop and be toFlatten[x]
+
+*/
