8f91780756aea9167c4e1e2c5befad7e13441354
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-10 09:07:24 -0800

    completed string-manipulation-3 exercise

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,52 +1,20 @@
 /* exported titleCase */
 /*
 return string with every word capitalized, excluding minor words, JavaScript, and API
-create variable to hold title case string
-make entire string lower case
-create array with 'minor' words
-loop through string chars
-check if char is a space
-if char is not a space, add it to temporary word container
-if char is a space, that means all chars prior to it made a word
-check if word is in minor words array
-if true, check if previous word ended in a colon
-if true, only add to the titleCase variable
-if previous word did not end in colon, and current word is in minor word array,
-  convert to lower case and add to titleCase variable
-continue loop to next interation
-if word is not in the minor array, check if word is Api or Javascript
-if they are, convert to API or JavaScript
-
+convert entire string into lower case letters
+separate the string if it has a colon (treat a colon as a start of new sentence)
+  (should return 2 arrays if there is a colon, 1 if there isn't)
+pass each array through a loop that capitalizes each word
+first word in string gets capitalized,
+any letter after a space gets capitalized
+any letter after a dash gets capitalized
+every other letter is lower case
+pass each array through a loop that checks for any 'minor' words, or if the word is
+  javascript or api
+need to create array containing 'minor' words
+skip the first word of each array (first word of each sentence)
+every word after gets checked and converted to lowercase if they match 'minor' word
 */
-
-// function titleCase(title) {
-//   var titleCase = '';
-//   var lowerTitle = title.toLowerCase();
-//   var emptyWord = title[0].toUpperCase();
-//   var minorWords = ['And', 'Or', 'But', 'A', 'An', 'The', 'As', 'At', 'By', 'For', 'In', 'Of', 'On', 'Per', 'To'];
-//   for (var x = 1; x < lowerTitle.length; x++) {
-//     if (lowerTitle[x] === ' ') {
-//       if (minorWords.includes(emptyWord) && titleCase.length > 1 && titleCase[titleCase.length - 2] !== ':') {
-//         emptyWord = emptyWord.toLowerCase();
-//       }
-//       titleCase += emptyWord + ' ';
-//       emptyWord = '';
-//       continue;
-//     }
-//     if (lowerTitle[x - 1] === ' ' || lowerTitle[x - 1] === '-') {
-//       emptyWord += lowerTitle[x].toUpperCase();
-//     } else {
-//       emptyWord += lowerTitle[x];
-//     }
-//     if (emptyWord === 'Javascript') {
-//       emptyWord = 'JavaScript';
-//     } else if (emptyWord === 'Api') {
-//       emptyWord = 'API';
-//     }
-//   }
-//   titleCase += emptyWord;
-//   return titleCase;
-// }
 
 function titleCase(title) {
   var lowerTitle = title.toLowerCase();
@@ -55,7 +23,7 @@ function titleCase(title) {
     titleCaseArray[x] = makeTitle(titleCaseArray[x]);
   }
   for (var y = 0; y < titleCaseArray.length; y++) {
-    titleCaseArray[y] = checkMinorWords(titleCaseArray[y]);
+    titleCaseArray[y] = checkSpecialWords(titleCaseArray[y]);
   }
   titleCaseArray = titleCaseArray.join(': ');
   return titleCaseArray;
@@ -73,7 +41,7 @@ function makeTitle(string) {
   return titleString;
 }
 
-function checkMinorWords(string) {
+function checkSpecialWords(string) {
   var minorWords = ['And', 'Or', 'But', 'A', 'An', 'The', 'As', 'At', 'By', 'For', 'In', 'Of', 'On', 'Per', 'To'];
   var split = string.split(' ');
   for (var x = 1; x < split.length; x++) {
