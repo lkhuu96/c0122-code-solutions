6a5f4d64283b25c84d3ef44ac39d475cc33be2b4
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-10 17:01:06 -0800

    typing tutor css in progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,1 +1,12 @@
-// var $p = document.querySelector('p');
+var $p = document.querySelector('p');
+
+function makeSpan(string) {
+  var createSpan = document.createElement('span');
+  createSpan.textContent = string;
+  return createSpan;
+}
+
+var word = 'grumpy wizard';
+for (var x = 0; x < word.length; x++) {
+  $p.appendChild(makeSpan(word[x]));
+}
