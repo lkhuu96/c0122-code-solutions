e0ba420f851cd081f1794ddee05356e534a07d48
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-10 22:06:58 -0800

    javascript-typing-tutor in progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,4 +1,13 @@
-var $p = document.querySelector('p');
+var $h1 = document.querySelector('h1');
+// var incorrectCounter = 0;
+var word = 'grumpy wizards make toxic brew';
+
+for (var x = 0; x < word.length; x++) {
+  $h1.appendChild(makeSpan(word[x]));
+}
+var $span = document.querySelectorAll('span');
+$span[0].className = 'current';
+document.addEventListener('keydown', checkKey);
 
 function makeSpan(string) {
   var createSpan = document.createElement('span');
@@ -6,7 +15,18 @@ function makeSpan(string) {
   return createSpan;
 }
 
-var word = 'grumpy wizard';
-for (var x = 0; x < word.length; x++) {
-  $p.appendChild(makeSpan(word[x]));
+function checkKey(event) {
+  for (var y = 0; y < $span.length; y++) {
+    if ($span[y].matches('.current') || $span[y].matches('.wrong')) {
+      if (event.key === $span[y].innerText) {
+        $span[y].className = 'correct';
+        $span[y + 1].className = 'current';
+        return;
+      } else {
+        $span[y].className = 'wrong';
+        // incorrectCounter++;
+        return;
+      }
+    }
+  }
 }
