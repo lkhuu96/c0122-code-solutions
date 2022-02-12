634b4279a19a4fa5a07b1b20d77cdfaf3c985ebd
Luc-Ky Khuu <khuu96@gmail.com>
2022-02-11 17:59:30 -0800

    array-manipulation-3 zip.js in progress

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,11 +1,25 @@
 /* exported flatten */
 /*
 unpack any passed arrays by 1 level
+create empty array container to hold new array values
 loop through original array
-if any index value is an array (toFlatten)
-splice original array at toFlatten index, delete its original position
-add the all toFlatten values
-the number to be inserted should be toFlatten.length
-the values to be inserted should be in a for loop and be toFlatten[x]
-
+if any index value is an array
+pass that array into another loop
+loop through the array within the array elements and add to array container
+else add the current array element to the new array container.
+return the new array.
 */
+
+function flatten(array) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (var j = 0; j < array[i].length; j++) {
+        newArray.push(array[i][j]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
