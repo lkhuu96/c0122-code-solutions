a05712baca14e3b3f7a54f88bf8b8c92b85f1be5
lkhuu96 <khuu96@gmail.com>
2022-03-17 14:43:59 -0700

    completed 'user can update a note' feature

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,25 +1,34 @@
 const fs = require('fs');
-const command = process.argv[2];
-const textInput = process.argv[3];
+const jsonObject = require('./data.json');
+const command = process.argv[2].toLowerCase();
+const firstArg = process.argv[3];
+const secondArg = process.argv[4];
+const notes = jsonObject.notes;
+const nextId = jsonObject.nextId;
 
 fs.readFile('data.json', 'utf8', (err, json) => {
   if (err) throw err;
-  const jsonObject = JSON.parse(json);
-  const notes = jsonObject.notes;
-  const nextId = jsonObject.nextId;
   if (command === 'read') {
     for (const key in notes) {
       console.log(`${key}: ${notes[key]}`);
     }
   } else if (command === 'create') {
-    notes[nextId] = textInput;
+    notes[nextId] = firstArg;
     jsonObject.nextId++;
   } else if (command === 'delete') {
-    if (notes[textInput]) {
-      delete notes[textInput];
+    if (notes[firstArg]) {
+      delete notes[firstArg];
     } else {
-      console.log(`No object with id '${textInput}' to delete`);
+      console.log(`No object with id '${firstArg}' to delete`);
     }
+  } else if (command === 'update') {
+    if (notes[firstArg]) {
+      notes[firstArg] = secondArg;
+    } else {
+      console.log(`No object with id '${firstArg}' to update`);
+    }
+  } else {
+    console.log('invalid command');
   }
   fs.writeFile('data.json', JSON.stringify(jsonObject, null, 2), err => {
     if (err) throw err;
