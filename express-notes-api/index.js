2bf119c9562d954ea3ededb9f54a04c2979bbaa8
lkhuu96 <khuu96@gmail.com>
2022-03-21 12:11:30 -0700

    clients can GET a single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const dataJSON = require('./data.json');
 const app = express();
 
+const errors = {
+  400: {
+    error: 'id must be a positive integer'
+  },
+  404: {
+    error: ''
+  }
+};
+
 app.get('/api/notes', (req, res) => {
   const notes = dataJSON.notes;
   const notesArray = [];
@@ -9,6 +18,19 @@ app.get('/api/notes', (req, res) => {
     notesArray.push(notes[id]);
   }
   res.status(200).send(notesArray);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const notes = dataJSON.notes;
+  const idNum = Number(req.params.id);
+  if (!Number.isInteger(req.params.id)) {
+    res.status(400).send(errors[400]);
+  } else if (notes[idNum]) {
+    res.status(200).send(notes[idNum]);
+  } else {
+    errors[404].error = `cannot find note with id ${idNum}`;
+    res.status(404).send(errors[404]);
+  }
 });
 
 app.listen(3000, () => {
