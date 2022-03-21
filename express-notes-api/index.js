f997cdad5eae9e989bd271d4e2d389d881710e66
lkhuu96 <khuu96@gmail.com>
2022-03-21 13:31:05 -0700

    Client can POST a new note

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const dataJSON = require('./data.json');
 const app = express();
+const fs = require('fs');
 
-const errors = {
-  400: {
-    error: 'id must be a positive integer'
-  },
-  404: {
-    error: ''
-  }
-};
+app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
   const notes = dataJSON.notes;
@@ -17,19 +11,39 @@ app.get('/api/notes', (req, res) => {
   for (const id in notes) {
     notesArray.push(notes[id]);
   }
-  res.status(200).send(notesArray);
+  res.status(200).json(notesArray);
 });
 
 app.get('/api/notes/:id', (req, res) => {
   const notes = dataJSON.notes;
   const idNum = Number(req.params.id);
   if (!Number.isInteger(req.params.id)) {
-    res.status(400).send(errors[400]);
+    res.status(400).json({ error: 'id must be a positive integer' });
   } else if (notes[idNum]) {
-    res.status(200).send(notes[idNum]);
+    res.status(200).json(notes[idNum]);
   } else {
-    errors[404].error = `cannot find note with id ${idNum}`;
-    res.status(404).send(errors[404]);
+    res.status(404).json({ error: `cannot find note with id ${idNum}` });
+  }
+});
+
+app.post('/api/notes', (req, res) => {
+  const noteContent = req.body;
+  const notes = dataJSON.notes;
+  const nextId = dataJSON.nextId;
+  if (!noteContent.content || !noteContent) {
+    res.status(400).json({ error: 'content is a required field' });
+  } else if (noteContent) {
+    notes[nextId] = noteContent;
+    notes[nextId].id = nextId;
+    dataJSON.nextId++;
+    fs.writeFile('data.json', JSON.stringify(dataJSON, null, 2), err => {
+      if (err) {
+        res.status(500).json({ error: 'An unexpected error has occured.' });
+        console.error(err);
+      } else {
+        res.status(201).json(notes[nextId]);
+      }
+    });
   }
 });
 
