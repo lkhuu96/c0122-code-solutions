77c7466234eaa3eee6f5bce594264993017bcf6a
lkhuu96 <khuu96@gmail.com>
2022-03-21 15:07:34 -0700

    client can delete a note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -17,7 +17,7 @@ app.get('/api/notes', (req, res) => {
 app.get('/api/notes/:id', (req, res) => {
   const notes = dataJSON.notes;
   const idNum = Number(req.params.id);
-  if (!Number.isInteger(req.params.id)) {
+  if (!Number.isInteger(idNum)) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (notes[idNum]) {
     res.status(200).json(notes[idNum]);
@@ -47,8 +47,24 @@ app.post('/api/notes', (req, res) => {
   }
 });
 
-app.delete('/api/notes:id', (req, res) => {
-
+app.delete('/api/notes/:id', (req, res) => {
+  const idNum = Number(req.params.id);
+  const notes = dataJSON.notes;
+  if (!Number.isInteger(idNum)) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (!notes[idNum]) {
+    res.status(404).json({ error: `cannot find note with id ${idNum}` });
+  } else if (notes[idNum]) {
+    delete notes[idNum];
+    fs.writeFile('data.json', JSON.stringify(dataJSON, null, 2), err => {
+      if (err) {
+        res.status(500).json({ error: 'An unexpected error has occurred.' });
+        console.error(err);
+      } else {
+        res.sendStatus(204);
+      }
+    });
+  }
 });
 
 app.listen(3000, () => {
