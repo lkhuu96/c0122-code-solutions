29f6d9a83038f8b44afa3813a2c3330d42d5152d
lkhuu96 <khuu96@gmail.com>
2022-03-25 10:21:59 -0700

    removed numbers array and names array in filter.js

--- array-filter/filter.js
+++ array-filter/filter.js
@@ -1,17 +1,3 @@
-const numbersArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-const namesArr = [
-  'Ada',
-  'Hedy',
-  'Jean',
-  'Grace',
-  'Evelyn',
-  'Joan',
-  'Elizabeth',
-  'Janese',
-  'Donna'
-];
-
 function filter(array, predicate) {
   const newArr = [];
   for (let i = 0; i < array.length; i++) {
