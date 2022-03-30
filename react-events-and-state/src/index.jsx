35fb9268fb5b3da3863de514fd576ae8845fcd08
lkhuu96 <khuu96@gmail.com>
2022-03-30 13:12:20 -0700

    completed react-events-and-state exercise

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(click => ({
+      isClicked: !this.state.isClicked
+    }));
+  }
+
+  render() {
+    if (this.state.isClicked) {
+      return (
+       <button onClick={this.handleClick}>Thanks</button>
+      );
+    } else {
+      return (
+        <button onClick={this.handleClick}>Click Me!</button>
+      );
+    }
+  }
+}
+const root = document.querySelector('#root');
+ReactDOM.render(<CustomButton />, root);
