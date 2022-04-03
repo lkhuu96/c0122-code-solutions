b4ae330466817327320a103f04ebb4ef83a9da59
lkhuu96 <khuu96@gmail.com>
2022-04-02 19:54:12 -0700

    completed react-validated-input-component exercise

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -4,35 +4,54 @@ class Input extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      statusMessage: null,
       input: '',
-      goodPassword: false
+      showError: false,
+      goodPassword: false,
+      showIcon: false
     };
     this.checkInput = this.checkInput.bind(this);
     this.returnFalse = this.returnFalse.bind(this);
     this.makeIcon = this.makeIcon.bind(this);
+    this.errorMessage = this.errorMessage.bind(this);
+    this.trackInput = this.trackInput.bind(this);
+  }
+
+  trackInput(event) {
+    this.setState({
+      input: event.target.value
+    });
+    this.checkInput(event);
   }
 
   checkInput(event) {
-    if (event.target.value.length < 8) {
+    if (event.target.value.length > 0 && event.target.value.length < 8) {
       this.setState({
-        input: event.target.value,
+        showError: true,
         goodPassword: false,
-        statusMessage: 'Your password is too short'
+        showIcon: true
+      });
+    } else if (event.target.value.length >= 8) {
+      this.setState({
+        showError: false,
+        goodPassword: true
       });
     } else {
       this.setState({
-        input: event.target.value,
-        goodPassword: true,
-        statusMessage: ''
+        showError: false,
+        showIcon: false
       });
+    }
+  }
+
+  errorMessage() {
+    if (!this.state.input) {
+      return 'A password is required';
+    } else {
+      return 'Your password is too short';
     }
   }
 
   makeIcon() {
-    if (!this.state.input) {
-      return null;
-    }
     return (
       <i className={this.state.goodPassword ? 'green fas fa-check' : 'red fas fa-times'}></i>
     );
@@ -40,10 +59,10 @@ class Input extends React.Component {
 
   returnFalse(event) {
     event.preventDefault();
-    if (!this.state.input) {
+    if (this.state.input.length < 1) {
       this.setState({
-        statusMessage: 'A password is required',
-        goodPassword: false
+        showError: true,
+        showIcon: true
       });
     }
   }
@@ -52,12 +71,12 @@ class Input extends React.Component {
     return (
       <form onSubmit={this.returnFalse}>
         <label htmlFor='password'>Password</label>
-          <div className="row center">
-            <input type="password" name='password' onChange={this.checkInput} autoComplete='false'></input>
-            {this.makeIcon()}
+          <div className="row align-center">
+            <input type="password" name='password' onChange={this.trackInput} autoComplete='false'></input>
+            {this.state.showIcon && this.makeIcon()}
           </div>
           <div className='red'>
-            {this.state.statusMessage}
+            {this.state.showError && this.errorMessage()}
           </div>
       </form>
     );
