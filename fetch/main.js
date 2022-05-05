b8ead73acacb68b6757d46159d40eca3c6f31a81
lkhuu96 <khuu96@gmail.com>
2022-05-04 17:22:02 -0700

    added async function to fetch practice

--- fetch/main.js
+++ fetch/main.js
@@ -1,4 +1,12 @@
 
-const fetchJson = fetch('https://jsonplaceholder.typicode.com/users', { method: 'GET' });
+// const fetchJson = fetch('https://jsonplaceholder.typicode.com/users', { method: 'GET' });
+// fetchJson.then(response => response.json()).then(data => console.log('fetch data:', data));
 
-fetchJson.then(response => response.json()).then(data => console.log('fetch data:', data));
+async function test() {
+  const fetchJson = fetch('https://jsonplaceholder.typicode.com/users', { method: 'GET' });
+  const result = await fetchJson;
+  const data = await result.json();
+  console.log('fetch data:', data);
+}
+
+test();
