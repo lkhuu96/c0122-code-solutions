1c0e656d0be002bf3f36026850ec20727922c623
lkhuu96 <khuu96@gmail.com>
2022-05-11 19:09:06 -0700

    refactored react-toggle-switch-component with hooks

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,43 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class ToggleSwitch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      toggle: false
+// class ToggleSwitch extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       toggle: false
+//     };
+//     this.clickToggle = this.clickToggle.bind(this);
+//   }
+
+//   clickToggle() {
+//     this.setState(() => ({
+//       toggle: !this.state.toggle
+//     }));
+//   }
+
+//   render() {
+//     let position = {};
+//     let switchColor = {};
+//     let label = 'OFF';
+//     if (this.state.toggle === true) {
+//       switchColor = {
+//         backgroundColor: 'green'
+//       };
+//       position = {
+//         right: 0
+//       };
+//       label = 'ON';
+//     }
+//     return (
+//       <div className='row col-gap'>
+//           <div className='row'>
+//             <div className="switch" style={switchColor}></div>
+//             <div onClick={this.clickToggle} style={position} className="circle"></div>
+//           </div>
+//           <p onClick={this.clickToggle}>{label}</p>
+//       </div>
+//     );
+//   }
+// }
+
+function ToggleSwitchHook() {
+  const [toggle, toggleSwitch] = useState(false);
+  let position = {};
+  let switchColor = {};
+  let label = 'OFF';
+  if (toggle === true) {
+    switchColor = {
+      backgroundColor: 'green'
     };
-    this.clickToggle = this.clickToggle.bind(this);
+    position = {
+      right: 0
+    };
+    label = 'ON';
   }
-
-  clickToggle() {
-    this.setState(() => ({
-      toggle: !this.state.toggle
-    }));
-  }
-
-  render() {
-    let position = {};
-    let switchColor = {};
-    let label = 'OFF';
-    if (this.state.toggle === true) {
-      switchColor = {
-        backgroundColor: 'green'
-      };
-      position = {
-        right: 0
-      };
-      label = 'ON';
-    }
-    return (
+  return (
       <div className='row col-gap'>
           <div className='row'>
             <div className="switch" style={switchColor}></div>
-            <div onClick={this.clickToggle} style={position} className="circle"></div>
+            <div onClick={() => toggleSwitch(!toggle)} style={position} className="circle"></div>
           </div>
-          <p onClick={this.clickToggle}>{label}</p>
+          <p onClick={() => toggleSwitch(!toggle)}>{label}</p>
       </div>
-    );
-  }
+  );
 }
-
-export default ToggleSwitch;
+export default ToggleSwitchHook;
