31486ab4a53961d31e8197d5ec13ff260b1a020e
lkhuu96 <khuu96@gmail.com>
2022-05-12 18:27:43 -0700

    refactored react-app-drawer-component with hooks

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const listItems = ['Home', 'Get Started', 'Sign In'];
 
@@ -8,41 +8,78 @@ function makeLists() {
   });
   return list;
 }
-class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      display: false
-    };
-    this.navToggle = this.navToggle.bind(this);
-  }
 
-  navToggle() {
-    this.setState({
-      display: !this.state.display
-    });
-  }
+function Modal() {
+  const [nav, setNav] = useState(false);
+  const [bg, setBg] = useState('fade-out');
+  const [modal, setModal] = useState('ease-out');
 
-  render() {
-    let modalClass = 'ease-in';
-    let bgClass = 'fade-in';
-    let canToggle = this.navToggle;
-    if (this.state.display === false) {
-      modalClass = 'ease-out';
-      bgClass = 'fade-out';
-      canToggle = function () {};
+  function toggleModal() {
+    if (nav === true) {
+      setNav(false);
+      setBg('fade-out');
+      setModal('ease-out');
+    } else {
+      setNav(true);
+      setBg('fade-in');
+      setModal('ease-in');
     }
-    return (
-      <div>
-        <div onClick={canToggle} className={`shade absolute ${bgClass}`}>
-        </div>
-        <i onClick={this.navToggle} className="fa-solid fa fa-bars absolute"></i>
-        <div className={`modal absolute ${modalClass}`}>
-          <h1>Menu</h1>
-          <ul onClick={this.navToggle}>{makeLists()}</ul>
-        </div>
-      </div>
-    );
   }
+
+  function toggleBg() {
+    if (bg === 'fade-in') {
+      setNav(false);
+      setBg('fade-out');
+      setModal('ease-out');
+    }
+  }
+  return (
+    <div>
+      <div onClick={() => toggleBg()} className={`shade absolute ${bg}`}>
+      </div>
+      <i onClick={() => toggleModal()} className="fa-solid fa fa-bars absolute"></i>
+      <div className={`modal absolute ${modal}`}>
+        <h1>Menu</h1>
+        <ul onClick={() => toggleModal()}>{makeLists()}</ul>
+      </div>
+    </div>
+  );
 }
+// class Modal extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       display: false
+//     };
+//     this.navToggle = this.navToggle.bind(this);
+//   }
+
+//   navToggle() {
+//     this.setState({
+//       display: !this.state.display
+//     });
+//   }
+
+//   render() {
+//     let modalClass = 'ease-in';
+//     let bgClass = 'fade-in';
+//     let canToggle = this.navToggle;
+//     if (this.state.display === false) {
+//       modalClass = 'ease-out';
+//       bgClass = 'fade-out';
+//       canToggle = function () {};
+//     }
+//     return (
+//       <div>
+//         <div onClick={canToggle} className={`shade absolute ${bgClass}`}>
+//         </div>
+//         <i onClick={this.navToggle} className="fa-solid fa fa-bars absolute"></i>
+//         <div className={`modal absolute ${modalClass}`}>
+//           <h1>Menu</h1>
+//           <ul onClick={this.navToggle}>{makeLists()}</ul>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 export default Modal;
