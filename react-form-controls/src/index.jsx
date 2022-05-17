5635561ca4e795f8102e8ee2c4531262a0e5ba30
lkhuu96 <khuu96@gmail.com>
2022-05-16 21:10:50 -0700

    refactored react-form-controls with hooks

--- react-form-controls/src/index.jsx
+++ react-form-controls/src/index.jsx
@@ -1,40 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-class NewsletterForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+// class NewsletterForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       email: ''
+//     };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
 
-  handleChange(event) {
-    this.setState({
-      email: event.target.value
-    });
-  }
+//   handleChange(event) {
+//     this.setState({
+//       email: event.target.value
+//     });
+//   }
 
-  handleSubmit(event) {
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     console.log('state:', this.state);
+//   }
+
+//   render() {
+//     return (
+//       <>
+//       <form onSubmit={this.handleSubmit}>
+//         <label >
+//             Email
+//             <input type='email' name='email' value={this.state.email} onChange={this.handleChange}></input>
+//         </label>
+//         <button>Sign Up</button>
+//       </form>
+//       </>
+//     );
+//   }
+// }
+
+function NewsletterForm() {
+  const [email, setEmail] = useState('');
+  function handleSubmit(event) {
     event.preventDefault();
-    console.log('state:', this.state);
+    console.log(email);
+    setEmail('');
   }
-
-  render() {
-    return (
-      <>
-      <form onSubmit={this.handleSubmit}>
-        <label >
-            Email
-            <input type='email' name='email' value={this.state.email} onChange={this.handleChange}></input>
-        </label>
-        <button>Sign Up</button>
-      </form>
-      </>
-    );
-  }
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+      <label >
+          Email
+          <input type='email' name='email' value={email} onChange={event => setEmail(event.target.value)}></input>
+      </label>
+      <button>Sign Up</button>
+    </form>
+    </>
+  );
 }
 
 ReactDOM.render(<NewsletterForm />, document.querySelector('#root'));
