8bb8432fed606fb027f6cfe05ca28afdea4ecce2
lkhuu96 <khuu96@gmail.com>
2022-06-01 14:39:55 -0700

    refactored app.jsx with hooks

--- react-context/client/app.jsx
+++ react-context/client/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppContext from './lib/app-context';
 import parseRoute from './lib/parse-route';
 import decodeToken from './lib/decode-token';
@@ -8,42 +8,34 @@ import NotFound from './pages/not-found';
 import Navbar from './components/navbar';
 import PageContainer from './components/page-container';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-      isAuthorizing: true,
-      route: parseRoute(window.location.hash)
-    };
-    this.handleSignIn = this.handleSignIn.bind(this);
-    this.handleSignOut = this.handleSignOut.bind(this);
-  }
+export default function App() {
+  const [user, setUser] = useState(null);
+  const [isAuthorizing, setAuthorizing] = useState(true);
+  const [route, setRoute] = useState(parseRoute(window.location.hash));
 
-  componentDidMount() {
+  useEffect(() => {
     window.addEventListener('hashchange', () => {
-      this.setState({
-        route: parseRoute(window.location.hash)
-      });
+      setRoute(parseRoute(window.location.hash));
     });
     const token = window.localStorage.getItem('react-context-jwt');
     const user = token ? decodeToken(token) : null;
-    this.setState({ user, isAuthorizing: false });
-  }
+    setUser(user);
+    setAuthorizing(false);
+  });
 
-  handleSignIn(result) {
+  const handleSignIn = result => {
     const { user, token } = result;
     window.localStorage.setItem('react-context-jwt', token);
-    this.setState({ user });
-  }
+    setUser(user);
+  };
 
-  handleSignOut() {
+  const handleSignOut = () => {
     window.localStorage.removeItem('react-context-jwt');
-    this.setState({ user: null });
-  }
+    setUser(null);
+  };
 
-  renderPage() {
-    const { path } = this.state.route;
+  const renderPage = () => {
+    const path = route.path;
     if (path === '') {
       return <Home />;
     }
@@ -51,22 +43,80 @@ export default class App extends React.Component {
       return <Auth />;
     }
     return <NotFound />;
-  }
+  };
 
-  render() {
-    if (this.state.isAuthorizing) return null;
-    const { user, route } = this.state;
-    const { handleSignIn, handleSignOut } = this;
-    const contextValue = { user, route, handleSignIn, handleSignOut };
-    return (
-      <AppContext.Provider value={contextValue}>
-        <>
-          <Navbar />
-          <PageContainer>
-            { this.renderPage() }
-          </PageContainer>
-        </>
-      </AppContext.Provider>
-    );
-  }
+  if (isAuthorizing) return null;
+  const contextValue = { user, route, handleSignIn, handleSignOut };
+  return (
+    <AppContext.Provider value={contextValue}>
+      <>
+        <Navbar />
+        <PageContainer>
+          { renderPage() }
+        </PageContainer>
+      </>
+    </AppContext.Provider>
+  );
 }
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       user: null,
+//       isAuthorizing: true,
+//       route: parseRoute(window.location.hash)
+//     };
+//     this.handleSignIn = this.handleSignIn.bind(this);
+//     this.handleSignOut = this.handleSignOut.bind(this);
+//   }
+
+//   componentDidMount() {
+//     window.addEventListener('hashchange', () => {
+//       this.setState({
+//         route: parseRoute(window.location.hash)
+//       });
+//     });
+//     const token = window.localStorage.getItem('react-context-jwt');
+//     const user = token ? decodeToken(token) : null;
+//     this.setState({ user, isAuthorizing: false });
+//   }
+
+//   handleSignIn(result) {
+//     const { user, token } = result;
+//     window.localStorage.setItem('react-context-jwt', token);
+//     this.setState({ user });
+//   }
+
+//   handleSignOut() {
+//     window.localStorage.removeItem('react-context-jwt');
+//     this.setState({ user: null });
+//   }
+
+//   renderPage() {
+//     const { path } = this.state.route;
+//     if (path === '') {
+//       return <Home />;
+//     }
+//     if (path === 'sign-in' || path === 'sign-up') {
+//       return <Auth />;
+//     }
+//     return <NotFound />;
+//   }
+
+//   render() {
+//     if (this.state.isAuthorizing) return null;
+//     const { user, route } = this.state;
+//     const { handleSignIn, handleSignOut } = this;
+//     const contextValue = { user, route, handleSignIn, handleSignOut };
+//     return (
+//       <AppContext.Provider value={contextValue}>
+//         <>
+//           <Navbar />
+//           <PageContainer>
+//             { this.renderPage() }
+//           </PageContainer>
+//         </>
+//       </AppContext.Provider>
+//     );
+//   }
+// }
